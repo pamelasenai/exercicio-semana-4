bfdb920c956dc8cab7bcda1b2ef5b7d453c334b7
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-01 01:36:11 -0300

    Exercício 2

--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,31 @@ public class Main {
     public static List<String> professores = new ArrayList<>();
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        byte opcao = 1;
+        popularListas();
+        byte opcao;
 
         imprimirBoasVindas();
 
         do {
-            imprimirMenu();
+            imprimirOpcoes();
             opcao = entrada.nextByte();
 
             switch (opcao){
                 case 0:
-                    System.out.println("\n--------------------------");
-                    System.out.println("Professores cadastrados: ");
-                    System.out.println("--------------------------");
-                    imprimirLista(professores);
-                    System.out.println("\n--------------------------");
-                    System.out.println("Cursos cadastrados: ");
-                    System.out.println("--------------------------");
-                    imprimirLista(listaCursos);
+                    imprimirLista(professores, "Professores");
+                    imprimirLista(listaCursos, "Cursos");
                     System.out.println("\nEncerrando...");
                     break;
                 case 1:
-                    System.out.println("\nVocê entrou no modulo de cadastramento de professor!");
-                    adicionarItemLista(professores);
-                    imprimirLista(professores);
+                    gerarMenu(professores, "Professores");
                     break;
                 case 2:
-                    System.out.println("\nVocê entrou no modulo de cadastramento de curso!");
-                    adicionarItemLista(listaCursos);
-                    imprimirLista(listaCursos);
+                    gerarMenu(listaCursos, "Cursos");
                     break;
                 default:
                     System.out.println("\nOpção invalida!");
                     break;
             }
-
         } while (opcao != 0);
 
         entrada.close();
@@ -55,24 +45,65 @@ public class Main {
                 "Esperamos que você aproveite ao máximo sua experiência aqui."
         );
     }
-
-    public static void imprimirMenu() {
+    public static void imprimirOpcoes() {
         System.out.print(
-                "--------------------------------------------------------------------\n" +
+                        "\n------------------------------------------------------\n" +
+                        "                        OPÇÕES                        \n" +
+                        "------------------------------------------------------\n" +
+                        "[1] Professor\n" +
+                        "[2] Curso\n" +
+                        "[0] Sair \n" +
+                        "------------------------------------------------------\n" +
+                        "Digite o número da opção que você deseja interagir: "
+        );
+    }
+
+    public static void imprimirMenu(String tipo) {
+        System.out.print(
+                "\n--------------------------------------------------------------------\n" +
                 "                                MENU                                \n" +
                 "--------------------------------------------------------------------\n" +
-                "[1] Adicionar professor \n" +
-                "[2] Adicionar curso \n" +
-                "[0] Sair \n" +
+                "[1] Listar " + tipo +
+                "\n[2] Adicionar " + tipo +
+                "\n[0] Sair \n" +
                 "--------------------------------------------------------------------\n" +
                 "Por favor, digite o número da opção do menu que você deseja acessar: "
         );
     }
 
-    public static void imprimirLista(List<String> lista) {
-        for (String item : lista){
-            System.out.println(item);
+    public static void imprimirLista(List<String> lista, String tipo) {
+        System.out.println("\n*************************");
+        System.out.println(tipo);
+        System.out.println("*************************");
+        for (int indice = 0; indice < lista.size(); indice++){
+            System.out.println(indice + "- " + lista.get(indice));
         }
+        System.out.println("*************************");
+    }
+
+    public static void gerarMenu(List<String> lista, String tipo) {
+        Scanner entrada = new Scanner(System.in);
+        byte opcao;
+        do {
+            imprimirMenu(tipo);
+            opcao = entrada.nextByte();
+
+            switch (opcao) {
+                case 0:
+                    imprimirLista(lista, tipo);
+                    System.out.println("\nVoltando ao menu anterior!");
+                    break;
+                case 1:
+                    imprimirLista(lista, tipo);
+                    break;
+                case 2:
+                    adicionarItemLista(lista);
+                    break;
+                default:
+                    System.out.println("\nOpção invalida!");
+                    break;
+            }
+        } while (opcao != 0);
     }
 
     public static void adicionarItemLista(List<String> lista) {
@@ -80,7 +111,7 @@ public class Main {
         String item;
 
         do {
-            System.out.print("Informe o item a ser cadastrado ou [s] para sair: ");
+            System.out.print("\nInforme o item a ser cadastrado ou [s] para sair: ");
             item = entrada.nextLine();
             if (item.equals("s")) {
                 System.out.println("Finalizando cadastro.");
@@ -97,5 +128,12 @@ public class Main {
             lista.add(item);
             System.out.println("Cadastrado realizado com sucesso.");
         } while (!item.equals("s"));
+    }
+
+    public static void popularListas() {
+        listaCursos.add("CSS");
+        listaCursos.add("JS");
+        professores.add("Pâmela");
+        professores.add("Rafael");
     }
 }
